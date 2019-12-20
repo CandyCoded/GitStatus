44d2c81e1de7218c5f9fb8e29e3f2fa3767074d8
Scott Doxey <neogeek0101@gmail.com>
2019-12-19 19:50:27 -0500

    Added GitStatus.

---
+++ Assets/Plugins/CandyCoded.GitStatus/Scripts/CustomEditor/GitStatus.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if UNITY_EDITOR
+using System;
+using UnityEditor;
+
+namespace CandyCoded.GitStatus
+{
+
+    [InitializeOnLoad]
+    public static class GitStatus
+    {
+
+        public static string branch;
+
+        public static string[] branches;
+
+        public static string[] changedFiles;
+
+        public static string[] untrackedFiles;
+
+        public static DateTime lastUpdated;
+
+        static GitStatus()
+        {
+
+            FileWatcher.UpdateEvent -= Update;
+            FileWatcher.UpdateEvent += Update;
+
+            Update();
+
+        }
+
+        public static void Update()
+        {
+
+            branch = Git.Branch();
+
+            branches = Git.Branches();
+
+            changedFiles = Git.ChangedFiles();
+
+            untrackedFiles = Git.UntrackedFiles();
+
+            lastUpdated = DateTime.Now;
+
+        }
+
+    }
+
+}
+#endif
